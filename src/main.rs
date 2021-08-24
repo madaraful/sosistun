1a22c4782caad7ace6cc80de35f5bf6bc59a31e5
Your Name <you@example.com>
2021-08-24 20:27:46 +0800

    a simple args parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use sosistab;
+use std::{env, process};
+use std::net::SocketAddr;
+
+fn server(listen: SocketAddr) {
+    // XXX: doing...
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    let mode:&str = match args.get(1) {
+        Some(v) => v,
+        None => {
+            eprintln!("why you do not input the running mode?");
+            process::exit(1);
+        }
+    };
+
+    match mode {
+        "client" => {},
+        "server" => {
+            let listen:&str = match args.get(2) {
+                Some(v) => v,
+                None => {
+                    eprintln!("fxxk! you want me to run as a server, but why you do not give me a listen address???");
+                    process::exit(3);
+                }
+            };
+
+            let listen:SocketAddr = match listen.parse() {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("are you an idiot? why not give me a right format of listen address???");
+                    process::exit(4);
+                }
+            };
+            println!("{:?}", listen);
+
+        },
+        _ => {
+            eprintln!("why you input a wrong mode???");
+            process::exit(2);
+        }
+    }
 }
