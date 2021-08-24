c3310eff605b629b67f9318bc455b46e9bf2c2ac
Your Name <you@example.com>
2021-08-24 21:02:44 +0800

    args parser 2.0

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use sosistab;
 use std::{env, process};
 use std::net::SocketAddr;
 
-fn server(listen: SocketAddr) {
+fn server(listen: SocketAddr, origin: SocketAddr) {
     // XXX: doing...
 }
 
@@ -22,11 +22,10 @@ fn main() {
             let listen:&str = match args.get(2) {
                 Some(v) => v,
                 None => {
-                    eprintln!("fxxk! you want me to run as a server, but why you do not give me a listen address???");
+                    eprintln!("fxxk. you want me to run as a server, but why you do not give me a listen address???");
                     process::exit(3);
                 }
             };
-
             let listen:SocketAddr = match listen.parse() {
                 Ok(v) => v,
                 Err(e) => {
@@ -34,8 +33,39 @@ fn main() {
                     process::exit(4);
                 }
             };
-            println!("{:?}", listen);
 
+            match args.get(3) {
+                Some(v) => {
+                    if v != "for" {
+                        eprintln!("maybe you really are an idiot. as I said, you can use \"for\" as the next argument's value.");
+                        process::exit(6);
+                    };
+                },
+                None => {
+                    eprintln!("I am a sosistab tunnel, not a server that like shadowsocks server. so why you do not give me a target address? what is the original server? you should use me like a kcptun server, not like a proxy server.");
+                    eprintln!("do you want me to help you? ok, you can use \"for\" as the next argument's value.");
+                    process::exit(5);
+                }
+            };
+
+            let origin:&str = match args.get(4) {
+                Some(v) => v,
+                None => {
+                    eprintln!("you are an idiot? as I said, what is the original server? if you don't have one, what is the reason of using me?");
+                    process::exit(7);
+                }
+            };
+            let origin:SocketAddr = match origin.parse() {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("why you give me a wrong format of original server's address???");
+                    process::exit(8);
+                }
+            };
+
+            println!("still in developing..."); process::exit(0);
+
+            //server(listen, );
         },
         _ => {
             eprintln!("why you input a wrong mode???");
