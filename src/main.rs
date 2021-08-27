fc26ba20666b0764fc747df3b9b6848d8330869c
Your Name <you@example.com>
2021-08-27 12:47:55 +0800

    zh

--- src/main.rs
+++ src/main.rs
@@ -119,7 +119,8 @@ async fn async_main() {
     let mode: &str = match args.get(1) {
         Some(v) => v,
         None => {
-            eprintln!("why you do not input the running mode?");
+            eprintln!("爲什麼你不告訴我該如何運行？請在本程序的第一個參數中寫上運行模式。");
+            //eprintln!("why you do not input the running mode?");
             process::exit(1);
         }
     };
@@ -129,14 +130,16 @@ async fn async_main() {
             let listen: &str = match args.get(2) {
                 Some(v) => v,
                 None => {
-                    eprintln!("you want me to run as a client, but why you do not give me a listen address???");
+                    eprintln!("你既然想讓我作爲客戶端運行，那爲什麼不給我一個TCP監聽地址？請在本程序的第二個參數中寫上TCP監聽地址。有了這個地址後，該地址就能作爲你的TCP客戶端的訪問點（該地址是直接聯通 Sosistun Server 上的 origin 地址的，訪問該地址就等於訪問origin地址）");
+                    //eprintln!("you want me to run as a client, but why you do not give me a listen address???");
                     process::exit(9);
                 }
             };
             let listen: SocketAddr = match listen.parse() {
                 Ok(v) => v,
                 Err(_e) => {
-                    eprintln!("why not give me a right format of listen address???");
+                    eprintln!("爲什麼不給我一個正確的格式呢？TCP監聽地址的格式不正確，他應該是 IP:Port 的格式（例如 127.0.0.1:1234 ）。請檢查本程序的第二個參數是否輸入正確。");
+                    //eprintln!("why not give me a right format of listen address???");
                     process::exit(10);
                 }
             };
@@ -144,15 +147,18 @@ async fn async_main() {
             match args.get(3) {
                 Some(v) => {
                     if v != "remote" {
-                        eprintln!(
+                        eprintln!("都說了讓你在第三個參數那裏寫 \"remote\" 了，爲什麼寫錯了？要用半角字符，而不是全角。請檢查本程序的第三個參數是否輸入正確。");
+                        /*eprintln!(
                             "as I said, you can use \"remote\" as the next argument's value."
-                        );
+                        );*/
                         process::exit(11);
                     };
                 }
                 None => {
-                    eprintln!("I am a sosistab tunnel, not a client like shadowsocks client. so why you do not give me a target? what is the remote server? you should use me like a kcptun client, not like a proxy client.");
-                    eprintln!("do you want me to help you? ok, you can use \"remote\" as the next argument's value.");
+                    eprintln!("你需要給我一個 Sosistun 服務器的地址，不然我怎麼知道服務器的地址是什麼呢？請在本程序的第四個參數中寫上 Sosistun 服務器的地址。本程序會將第二個參數那裏的地址給轉發到 Sosistun 服務器的 origin 地址。而你需要提供給我： Sosistun服務器  的可訪問IP + Sosistun服務器程序  的第二個參數的端口。");
+                    eprintln!("你應該使用 \"remote\" 作爲第三個參數的值。");
+                    //eprintln!("I am a sosistab tunnel, not a client like shadowsocks client. so why you do not give me a target? what is the remote server? you should use me like a kcptun client, not like a proxy client.");
+                    //eprintln!("do you want me to help you? ok, you can use \"remote\" as the next argument's value.");
                     process::exit(12);
                 }
             };
@@ -160,14 +166,16 @@ async fn async_main() {
             let remote: &str = match args.get(4) {
                 Some(v) => v,
                 None => {
-                    eprintln!("as I said, what is the remote server? if you don't have one, what is the reason of using me?");
+                    eprintln!("你確實在第三個參數中寫上了 \"remote\"，但是你應該在第四個參數中寫上 Sosistun服務器 的地址。如果你沒有服務器地址，就無法使用我（畢竟瀏覽器如果沒有URL那該怎麼訪問網站呢？就是同樣的道理）");
+                    //eprintln!("as I said, what is the remote server? if you don't have one, what is the reason of using me?");
                     process::exit(13);
                 }
             };
             let remote: SocketAddr = match remote.parse() {
                 Ok(v) => v,
                 Err(_e) => {
-                    eprintln!("why you give me a invalid format of remote server's address???");
+                    eprintln!("爲什麼不給我一個正確的格式呢？Sosistun服務器 地址的格式不正確，他應該是 IP:Port 的格式（例如 127.0.0.1:1234 ）。請檢查本程序的第四個參數是否輸入正確。");
+                    //eprintln!("why you give me a invalid format of remote server's address???");
                     process::exit(14);
                 }
             };
@@ -175,13 +183,16 @@ async fn async_main() {
             match args.get(5) {
                 Some(v) => {
                     if v != "pubkey" {
-                        eprintln!("you made a mistake, please retype it again.");
+                        eprintln!("都說了讓你在第五個參數那裏寫 \"pubkey\" 了，爲什麼寫錯了？要用半角字符，而不是全角。請檢查本程序的第五個參數是否輸入正確。");
+                        //eprintln!("you made a mistake, please retype it again.");
                         process::exit(17);
                     }
                 }
                 None => {
-                    eprintln!("good, you did it right. next step, give me a server's public key.");
-                    eprintln!("you can use \"pubkey\" as next arguments.");
+                    eprintln!("good，你做的好，目前輸入的參數都是有效的格式。下一步，請給我 Sosistun服務器 的 public key（也就是公鑰），就輸入在第六個參數即可。");
+                    eprintln!("你應該使用 \"pubkey\" 作爲第五個參數的值。");
+                    /*eprintln!("good, you did it right. next step, give me a server's public key.");
+                    eprintln!("you can use \"pubkey\" as next arguments.");*/
                     process::exit(16);
                 }
             };
@@ -190,7 +201,8 @@ async fn async_main() {
                 Some(v) => {
                     let v_len = v.len();
                     if (v_len % 2) != 0 {
-                        eprintln!("the format of public key is hex string. please make sure you entered correct.");
+                        eprintln!("公鑰的格式是 hex，請檢查本程序的第六個參數是否輸入正確。");
+                        //eprintln!("the format of public key is hex string. please make sure you entered correct.");
                         process::exit(19);
                     };
 
@@ -200,21 +212,24 @@ async fn async_main() {
                         match accept_chars.find(it) {
                             Some(_) => {}
                             None => {
-                                eprintln!("the format of public key is hex string, and the letters must be capital. please make sure you entered correct.");
+                                eprintln!("公鑰的格式是 hex，而且字母必須用大寫字母。請檢查本程序的第六個參數是否輸入正確。");
+                                //eprintln!("the format of public key is hex string, and the letters must be capital. please make sure you entered correct.");
                                 process::exit(20);
                             }
                         }
                     }
 
                     if v_len != (32 * 2) {
-                        eprintln!("the length of public key is must be 32 bytes. please make sure you entered correct.");
+                        eprintln!("公鑰的長度必須是 32 字節，請檢查本程序的第六個參數是否輸入正確。");
+                        //eprintln!("the length of public key is must be 32 bytes. please make sure you entered correct.");
                         process::exit(21);
                     }
 
                     v.to_string()
                 }
                 None => {
-                    eprintln!("please type the public key as the next argument's value.");
+                    eprintln!("你不給我公鑰那我怎麼連接 Sosistun服務器 呢？請在本程序的第六個參數中寫上 Sosistun服務器 的公鑰。");
+                    //eprintln!("please type the public key as the next argument's value.");
                     process::exit(18);
                 }
             };
@@ -239,14 +254,16 @@ async fn async_main() {
             let listen: &str = match args.get(2) {
                 Some(v) => v,
                 None => {
-                    eprintln!("you want me to run as a server, but why you do not give me a listen address???");
+                    eprintln!("你既然想讓我作爲服務器運行，那爲什麼不給我一個 Sosistab監聽地址？請在本程序的第二個參數中寫上 Sosistab監聽地址。該地址對應於 Sosistun客戶端 的 remote 地址。");
+                    //eprintln!("you want me to run as a server, but why you do not give me a listen address???");
                     process::exit(3);
                 }
             };
             let listen: SocketAddr = match listen.parse() {
                 Ok(v) => v,
                 Err(_e) => {
-                    eprintln!("why not give me a right format of listen address???");
+                    eprintln!("爲什麼不給我一個正確的格式呢？Sosistab監聽地址 的格式不正確，他應該是 IP:Port 的格式（例如 127.0.0.1:1234 ）。請檢查本程序的第二個參數是否輸入正確。");
+                    //eprintln!("why not give me a right format of listen address???");
                     process::exit(4);
                 }
             };
@@ -254,15 +271,20 @@ async fn async_main() {
             match args.get(3) {
                 Some(v) => {
                     if v != "origin" {
+                        eprintln!("都說了讓你在第三個參數那裏寫 \"origin\" 了，爲什麼寫錯了？要用半角字符，而不是全角。請檢查本程序的第三個參數是否輸入正確。");
+                        /*
                         eprintln!(
                             "as I said, you can use \"origin\" as the next argument's value."
                         );
+                        */
                         process::exit(6);
                     };
                 }
                 None => {
-                    eprintln!("I am a sosistab tunnel, not a server like shadowsocks server. so why you do not give me a target? what is the original server? you should use me like a kcptun server, not like a proxy server.");
-                    eprintln!("do you want me to help you? ok, you can use \"origin\" as the next argument's value.");
+                    eprintln!("你需要給我一個 原始TCP服務器 的地址，不然我怎麼知道 TCP服務器 的地址是什麼呢？請在本程序的第四個參數中寫上 原始TCP服務器的地址。本程序會將第二個參數那裏的入站連接給轉發到 原始TCP服務器 的地址。而你需要提供給我：原始TCP服務器的可訪問IP和端口。");
+                    eprintln!("你應該使用 \"origin\" 作爲第三個參數的值。");
+                    //eprintln!("I am a sosistab tunnel, not a server like shadowsocks server. so why you do not give me a target? what is the original server? you should use me like a kcptun server, not like a proxy server.");
+                    //eprintln!("do you want me to help you? ok, you can use \"origin\" as the next argument's value.");
                     process::exit(5);
                 }
             };
@@ -270,14 +292,16 @@ async fn async_main() {
             let origin: &str = match args.get(4) {
                 Some(v) => v,
                 None => {
-                    eprintln!("as I said, what is the original server? if you don't have one, what is the reason of using me?");
+                    eprintln!("你確實在第三個參數中寫上了 \"origin\"，但是你應該在第四個參數中寫上 原始TCP服務器 的地址。如果你沒有服務器地址，就無法使用我（畢竟 netcat 如果沒有地址那該怎麼訪問目標主機呢？就是同樣的道理）");
+                    //eprintln!("as I said, what is the original server? if you don't have one, what is the reason of using me?");
                     process::exit(7);
                 }
             };
             let origin: SocketAddr = match origin.parse() {
                 Ok(v) => v,
                 Err(_e) => {
-                    eprintln!("why you give me a invalid format of original server's address???");
+                    eprintln!("爲什麼不給我一個正確的格式呢？原始TCP服務器 地址的格式不正確，他應該是 IP:Port 的格式（例如 127.0.0.1:1234 ）。請檢查本程序的第四個參數是否輸入正確。");
+                    //eprintln!("why you give me a invalid format of original server's address???");
                     process::exit(8);
                 }
             };
@@ -300,14 +324,17 @@ async fn async_main() {
             };
             assert!((pubkey_hex.len() % 2) == 0);
 
+            println!("你的客戶端程序可以使用這個 public key（也就是公鑰） 來連接本服務器：{}", &pubkey_hex);
+            /*
             println!(
                 "Your clients can use this public key to connect this server: {}",
                 &pubkey_hex
-            );
+            );*/
             server(key, listen, origin).await;
         }
         _ => {
-            eprintln!("why you type a invalid mode???");
+            eprintln!("爲什麼你給我一個無效的運行模式？運行模式不是 client 就是 server，沒有第三個模式。");
+            //eprintln!("why you type a invalid mode???");
             process::exit(2);
         }
     }
